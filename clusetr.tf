3e5aa2aac4439f529d8ebc777b45614c694488f8
Ganesh Arja <jayaganesharja.com>
2024-05-21 11:41:42 +0530

    Auto push

---
+++ clusetr.tf
@@ -0,0 +1,19 @@
+# Creates DOCDB cluster
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "roboshop-${var.ENV}-docdb"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true                          # Value will be false in production. In lab, we will be using true
+
+
+# Creates DocDB Subnet Group
+resource "aws_docdb_subnet_group" "docdb" {
+  name       = "roboshop-${var.ENV}-docdb-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS 
+
+  tags = {
+    Name = "roboshop-${var.ENV}-docdb-subnet-group"
+  }
+}
